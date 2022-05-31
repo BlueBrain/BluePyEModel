a87ba0fba736399822700b0de80ca24746cccb47
Tanguy Pierre Louis Damart <tanguy.damart@epfl.ch>
2022-05-31 10:56:32 +0200

    Make threshold_efeature_std independent for all efeatures

--- bluepyemodel/efeatures_extraction/efeatures_extraction.py
+++ bluepyemodel/efeatures_extraction/efeatures_extraction.py
@@ -89,11 +89,12 @@ def extract_save_features_protocols(access_point, mapper=map):
     fitness_calculator_config = FitnessCalculatorConfiguration(
         name_rmp_protocol=access_point.pipeline_settings.name_rmp_protocol,
         name_rin_protocol=access_point.pipeline_settings.name_Rin_protocol,
-        threshold_efeature_std=access_point.pipeline_settings.threshold_efeature_std,
         validation_protocols=access_point.pipeline_settings.validation_protocols,
     )
 
-    fitness_calculator_config.init_from_bluepyefe(efeatures, stimuli, current)
+    fitness_calculator_config.init_from_bluepyefe(
+        efeatures, stimuli, current, access_point.pipeline_settings.threshold_efeature_std
+    )
 
     access_point.store_fitness_calculator_configuration(fitness_calculator_config)
 
