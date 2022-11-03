bfa7678e49a7569ae6a6135f8d86e1f88256ee00
Jaquier Aurélien Tristan <aurelien.jaquier@epfl.ch>
2022-11-03 11:26:29 +0100

    added test_format_protocol_name_to_list
    Change-Id: I493f66ad6673c0843bb639c72bf5c215945979db

--- tests/unit_tests/test_tools.py
+++ tests/unit_tests/test_tools.py
@@ -1,6 +1,10 @@
 from pathlib import Path
 
+import pytest
+
 from bluepyemodel.tools.mechanisms import get_mechanism_currents
+from bluepyemodel.tools.utils import are_same_protocol
+from bluepyemodel.tools.utils import format_protocol_name_to_list
 
 TEST_ROOT = Path(__file__).parents[1]
 DATA = TEST_ROOT / "test_data"
@@ -19,3 +23,31 @@ def test_get_mechanism_currents():
     ion_currs, nonspec_currs = get_mechanism_currents(DATA / "mechanisms" / "Ih.mod")
     assert ion_currs == []
     assert nonspec_currs == ["ihcn"]
+
+
+def test_format_protocol_name_to_list():
+    # str case
+    name, amp = format_protocol_name_to_list("APWaveform_140")
+    assert name == "APWaveform"
+    assert amp == 140.0
+
+    name, amp = format_protocol_name_to_list("APWaveform_140.0")
+    assert name == "APWaveform"
+    assert amp == 140.0
+
+    name, amp = format_protocol_name_to_list("APWaveform")
+    assert name == "APWaveform"
+    assert amp is None
+
+    # list case
+    name, amp = format_protocol_name_to_list(["APWaveform", 140])
+    assert name == "APWaveform"
+    assert amp == 140.0
+
+    # error case
+    with pytest.raises(TypeError, match="protocol_name should be a string or a list."):
+        format_protocol_name_to_list(None)
+
+
+def test_are_same_protocol():
+
