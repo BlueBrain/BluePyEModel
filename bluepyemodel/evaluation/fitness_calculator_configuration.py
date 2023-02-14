652ade235544b4597b9ef94a1d220f5fd290238e
arnaudon <alexis.arnaudon@epfl.ch>
2023-02-14 17:38:48 +0100

    improve search params

--- bluepyemodel/evaluation/fitness_calculator_configuration.py
+++ bluepyemodel/evaluation/fitness_calculator_configuration.py
@@ -110,13 +110,13 @@ class FitnessCalculatorConfiguration:
             ion_variables (list of str): ion current names and ionic concentration anmes
                 for all available mechanisms
         """
-
-        self.rmp_duration = 500.0
-        self.rin_step_delay = 500.0
-        self.rin_step_duration = 500.0
-        self.rin_step_amp = -0.02
-        self.rin_totduration = 1000.0
-        self.search_holding_duration = 1000.0
+        # I have adjusted parameters here so that the traces are stable to compute constant voltages
+        self.rmp_duration = 2000.0
+        self.rin_step_delay = 2000.0
+        self.rin_step_duration = 3000.0
+        self.rin_step_amp = -0.001
+        self.rin_totduration = self.rin_step_delay + self.rin_step_duration
+        self.search_holding_duration = 2000.0
         self.search_threshold_step_delay = 500.0
         self.search_threshold_step_duration = 2000.0
         self.search_threshold_totduration = 3000.0
