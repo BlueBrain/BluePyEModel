d6a129899d1638d712aac5bd0a58595477ae4a1a
ilkilic <ilkan.kilic@epfl.ch>
2023-10-09 19:09:49 +0200

    fix analysis.sh script

--- examples/emodel_pipeline_local_python/analysis.sh
+++ examples/emodel_pipeline_local_python/analysis.sh
@@ -7,4 +7,4 @@ export GITHASH="YOUR_GITHASH_HERE"
 
 export RUNLISTPATH="`pwd`/logs/analyse_list.txt"
 
-sbatch -J analysis_${OPT_EMODEL}_${GITHASH} ./analysis.sbatch
+sbatch -J "analysis_${OPT_EMODEL}_${GITHASH}" ./analysis.sbatch
