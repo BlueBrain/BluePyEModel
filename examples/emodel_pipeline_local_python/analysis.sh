a42d88b151ee226acb13aedc89e31f86c9daab83
ilkilic <ilkan.kilic@epfl.ch>
2023-10-11 12:11:03 +0200

    fix batch scripts

--- examples/emodel_pipeline_local_python/analysis.sh
+++ examples/emodel_pipeline_local_python/analysis.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+#####################################################################
 # Copyright 2023, EPFL/Blue Brain Project
 
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,8 +14,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#####################################################################
 
-#!/bin/bash
 
 source ./myvenv/bin/activate
 
--- examples/emodel_pipeline_local_python/create_venv.sh
+++ examples/emodel_pipeline_local_python/create_venv.sh
@@ -1,3 +1,4 @@
+#####################################################################
 # Copyright 2023, EPFL/Blue Brain Project
 
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,6 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#####################################################################
 
 deactivate
 module purge all # comment out this line if you are not using spack packages
