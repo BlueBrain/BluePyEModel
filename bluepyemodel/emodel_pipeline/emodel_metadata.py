2a88793d20b803f0715a3b0c9cf33e38c5c2dbed
ilkilic <ilkan.kilic@epfl.ch>
2023-12-11 14:19:40 +0100

    refactor check_emodel_name

--- bluepyemodel/emodel_pipeline/emodel_metadata.py
+++ bluepyemodel/emodel_pipeline/emodel_metadata.py
@@ -56,9 +56,8 @@ class EModelMetadata:
         if emodel is None and etype is None:
             raise ValueError("At least emodel or etype should be informed")
 
-        self.check_name(emodel)  # check if name complies with requirements
-
         self.emodel = emodel
+        self.check_emodel_name()  # check if name complies with requirements
         self.etype = None if etype == "None" else etype
         self.ttype = None if ttype == "None" else ttype
         self.mtype = None if mtype == "None" else mtype
@@ -184,7 +183,7 @@ class EModelMetadata:
 
         return s[:-2]
 
-    def check_name(self, emodel_name):
+    def check_emodel_name(self):
         """Check if name complies with requirements:
         https://nrn.readthedocs.io/en/8.2.3/guide/hoc_chapter_11_old_reference.html#names"""
 
@@ -192,12 +191,12 @@ class EModelMetadata:
         translate_args = str.maketrans("", "", allowed_chars)
 
         if (
-            emodel_name == ""
-            or emodel_name[0] not in string.ascii_letters
-            or not str(emodel_name).translate(translate_args) == ""
+            self.emodel == ""
+            or self.emodel[0] not in string.ascii_letters
+            or not str(self.emodel).translate(translate_args) == ""
         ):
             raise TypeError(
-                f"Emodel: name {emodel_name} provided to constructor does not comply "
+                f"Emodel: name {self.emodel} provided to constructor does not comply "
                 "with the rules for Neuron template name: name should be "
                 "alphanumeric "
                 "non-empty string, underscores are allowed, "
