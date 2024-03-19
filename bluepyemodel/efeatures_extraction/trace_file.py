b91240dcd41a575838790b2fe19eff9901fe74af
Jaquier Aurélien Tristan <aurelien.jaquier@epfl.ch>
2024-03-19 14:29:31 +0100

    add docstring to TraceFile.__init__
    Change-Id: I921ea2e5a7f6ab4f748abea1bad6dc935def53b2

--- bluepyemodel/efeatures_extraction/trace_file.py
+++ bluepyemodel/efeatures_extraction/trace_file.py
@@ -63,7 +63,51 @@ class TraceFile:
         etype=None,
         id=None,
     ):
-        """Docstring please"""
+        """Constructor.
+        
+        Args:
+            cell_name (str): cell name
+            filename (str): file name
+            filepath (str): file path
+            resource_id (str): deprecated. kept for legacy reasons
+            ecodes (dict): the ecodes, e.g.
+
+                .. code-block::
+
+                    {
+                        "IDrest": {
+                            "t_unit": "s",
+                            "ljp": 14.0,
+                            "protocol_name": "IDrest"
+                    }
+            other_metadata (dict): voltage and current files, e.g.
+
+                .. code-block::
+
+                    {
+                        "v_file": "path_to_voltage_file",
+                        "i_file": "path_to_current_file"
+                    }
+
+            species (dict): species, e.g.
+
+                .. code-block::
+
+                    {
+                        "id": "http://purl.obolibrary.org/obo/NCBITaxon_10090",
+                        "label": "Mus musculus",
+                    }
+            brain_region (dict): brain region, e.g.
+
+                .. code-block::
+
+                    {
+                        "@id": "http://api.brain-map.org/api/v2/data/Structure/407",
+                        "label": "Field CA1, pyramidal layer"
+                    }
+            etype (str): etype
+            id (str): the nexus id of the trace resource
+        """
         self.cell_name = cell_name
         self.filename = filename if filename else cell_name
         self.filepath = filepath
