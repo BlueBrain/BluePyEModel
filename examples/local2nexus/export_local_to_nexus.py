301cfaec68d78aae3c77ab2c866e975e80546331
ilkilic <ilkan.kilic@epfl.ch>
2024-05-13 10:58:27 +0200

    update local2nexus example

--- examples/local2nexus/export_local_to_nexus.py
+++ examples/local2nexus/export_local_to_nexus.py
@@ -1,37 +1,40 @@
 """Upload local models to nexus."""
+
 import getpass
 import os
-from bluepyemodelnexus.export_emodel import export_emodels_nexus
+from bluepyemodel.access_point.export_emodel import export_emodels_nexus
 from bluepyemodel.access_point.local import LocalAccessPoint
 import logging
 
 logger = logging.getLogger(__name__)
 
 # Please change the following settings according to your needs and data:
-emodel = "YOUR_EMODEL_NAME_HERE" # name of the local emodel you want to upload as it appears in the config/recipes.json file
-githash = "YOUR_GITHASH_HERE" # githash of the local emodel (provided during the optimization step).
-only_validated = False # only upload validated emodels
+emodel = "YOUR_EMODEL_NAME_HERE"  # name of the local emodel you want to upload as it appears in the config/recipes.json file
+githash = (
+    "YOUR_GITHASH_HERE"  # githash of the local emodel (provided during the optimization step).
+)
+only_validated = False  # only upload validated emodels
 
-only_best = False # only upload best emodel
-seeds = [1] # list of seeds that you want to upload, Please leave it empty if only_best=True.
+only_best = False  # only upload best emodel
+seeds = [1]  # list of seeds that you want to upload, Please leave it empty if only_best=True.
 
 # should match the data of your LocalAccessPoint emodel, if it was not set, use None
 etype = None
 mtype = None
 ttype = None
-species = "rat" # e.g. "mouse"
-brain_region = "SSCX" # e.g. "SSCX"
+species = "rat"  # e.g. "mouse"
+brain_region = "SSCX"  # e.g. "SSCX"
 
 description = ""
 
 # Nexus settings
-nexus_project = "mmb-emodels-for-synthesized-neurons" # a valid Nexus project name to which the emodel should be uploaded.
-nexus_organisation = "bbp" # choose between "bbp" or "public"
+nexus_project = "mmb-emodels-for-synthesized-neurons"  # a valid Nexus project name to which the emodel should be uploaded.
+nexus_organisation = "bbp"  # choose between "bbp" or "public"
 # Nexus advanced settings (only change if you know what you are doing)
 nexus_endpoint = "https://bbp.epfl.ch/nexus/v1"
 forge_path = "./forge.yml"
 forge_ontology_path = "./nsg.yml"
-sleep_time = 10 # increase the delay in case indexing is slow
+sleep_time = 10  # increase the delay in case indexing is slow
 
 
 def main():
@@ -58,8 +61,8 @@ def main():
         local_access_point,
         nexus_organisation=nexus_organisation,
         nexus_project=nexus_project,
-        nexus_endpoint = nexus_endpoint,
-        forge_path = forge_path,
+        nexus_endpoint=nexus_endpoint,
+        forge_path=forge_path,
         access_token=access_token,
         only_validated=only_validated,
         only_best=only_best,
@@ -68,6 +71,7 @@ def main():
         forge_ontology_path=forge_ontology_path,
         sleep_time=sleep_time,
     )
+
 
 if __name__ == "__main__":
     main()
