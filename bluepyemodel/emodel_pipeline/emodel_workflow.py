fd5f9d7c57f7719df3a05a78be1c453a9b7bb7c7
ilkilic <ilkan.kilic@epfl.ch>
2024-06-18 18:17:18 +0200

    update copyright

--- bluepyemodel/emodel_pipeline/emodel_workflow.py
+++ bluepyemodel/emodel_pipeline/emodel_workflow.py
@@ -1,7 +1,7 @@
 """EModelWorkflow class"""
 
 """
-Copyright 2024, EPFL/Blue Brain Project
+Copyright 2023-2024 Blue Brain Project / EPFL
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
--- bluepyemodel/tools/search_pdfs.py
+++ bluepyemodel/tools/search_pdfs.py
@@ -2,7 +2,7 @@
 of the emodel pipeline"""
 
 """
-Copyright 2024, EPFL/Blue Brain Project
+Copyright 2023-2024 Blue Brain Project / EPFL
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
