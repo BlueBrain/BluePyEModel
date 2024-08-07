0f7f9ff745bc50e95296033bed57f49efdb350ca
Jaquier Aurélien Tristan <aurelien.jaquier@epfl.ch>
2024-08-07 17:44:33 +0200

    fix for new recoding type in configuration
    Change-Id: Idb40795269a9ef6c580f2175e5354d38e61766e5

--- bluepyemodel/evaluation/protocol_configuration.py
+++ bluepyemodel/evaluation/protocol_configuration.py
@@ -109,7 +109,9 @@ class ProtocolConfiguration:
             new_recordings.append(recording)
             new_recordings_from_config.append(recording)
 
-            if ion_variables is not None:
+            if ion_variables is not None and recording["type"] not in (
+                "FixedDtRecordingStimulus", "LooseDtRecordingStimulus"
+            ):
                 for ion in ion_variables:
                     new_rec = recording.copy()
 
